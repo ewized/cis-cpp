66d6061d66d21f3869704c080bcc38de3a2b62b9
Joshua Rodriguez <ewized@gmail.com>
2013-10-22 11:39:15 -0700

    Create rainfall program

---
+++ rainfall/rainfall.cpp
@@ -0,0 +1,20 @@
+/*
+    Filename: rainfall.cpp
+    Problem: #1 pg 429
+    Description: Write a program that reads in the average monthly rainfall
+        for a city for each month of the year and then reads in the actual
+        monthly rainfall for each of the previous 12 months. The program
+        then prints out a nicely formated table show the rainfall for each
+        of the previous 12 months as well as how much above or bellow the
+        average the rain fall for each months.
+    Author: Joshua Rodriguez
+    Email: ewized@gmail.com
+    Date: 10/22/2013
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ rainfall/test.cpp
@@ -0,0 +1,15 @@
+/*
+    Filename: test.cpp
+    Problem:
+    Description: 
+    Author: Joshua Rodriguez
+    Email: ewized@gmail.com
+    Date: 10/22/2013
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
